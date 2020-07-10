7a20263d054b8132d0be0823796e1abed0e27d34
lucassherwin <lksherwin@gmail.com>
2020-07-10 18:27:47 -0400

    display weather info

--- src/components/CurrentDay.js
+++ src/components/CurrentDay.js
@@ -8,8 +8,10 @@ export class CurrentDay extends Component {
         currentTempC: null,
         isRaining: null, //this will be a bool
         seeMoreLink: null,
-        tempHigh: null, //high for the day
-        tempLow: null, //low for the day
+        tempHighF: null, //high for the day
+        tempHighC: null, 
+        tempLowF: null, //low for the day
+        tempLowC: null,
         humidity: null,
         uvIndex: null,
         uvIndexText: null,
@@ -31,6 +33,19 @@ export class CurrentDay extends Component {
             currentTempC: data[0]['Temperature']['Metric']['Value'],
             isRaining: data[0]['HasPrecipitation'],
             seeMoreLink: data[0]['Link'],
+            tempHighF: data[0]['TemperatureSummary']['Past24HourRange']['Maximum']['Imperial']['Value'],
+            tempHighC: data[0]['TemperatureSummary']['Past24HourRange']['Maximum']['Metric']['Value'],
+            tempLowF: data[0]['TemperatureSummary']['Past24HourRange']['Minimum']['Imperial']['Value'],
+            tempLowC: data[0]['TemperatureSummary']['Past24HourRange']['Minimum']['Metric']['Value'],
+            humidity: data[0]['RelativeHumidity'],
+            uvIndex: data[0]['UVIndex'],
+            uvIndexText: data[0]['UVIndexText'],
+            windDirection: data[0]['Wind']['Direction']['Localized'],
+            windSpeedMPH: data[0]['Wind']['Speed']['Imperial']['Value'],
+            windSpeedKMPH: data[0]['Wind']['Speed']['Metric']['Value'],
+            windChillTempC: data[0]['WindChillTemperature']['Metric']['Value'],
+            windChillTempF: data[0]['WindChillTemperature']['Imperial']['Value']
+
             
         }))
     }
@@ -40,6 +55,22 @@ export class CurrentDay extends Component {
             <div>
                 <h1>current day</h1>
                 <p>The Current Weather Is: {this.state.currentWeatherType}</p>
+                <p>currentTempF: {this.state.currentTempF}</p>
+                <p>currentTempC: {this.state.currentTempC}</p>
+        <p>isRaining: {this.state.isRaining ? 'Currently Raining' : 'Not Currently Raining'}</p>
+        <p>seeMoreLink: {this.state.seeMoreLink}</p>
+        <p>tempHighF: {this.state.tempHighF}</p>
+        <p>tempHighC: {this.state.tempHighC}</p>
+        <p>tempLowF: {this.state.tempLowF}</p>
+        <p>tempLowC: {this.state.tempLowC}</p>
+        <p>humidity: {this.state.humidity}</p>
+        <p>uvIndex: {this.state.uvIndex}</p>
+        <p>uvIndexText: {this.state.uvIndexText}</p>
+        <p>windDirection: {this.state.windDirection}</p>   
+        <p>windSpeedMPH: {this.state.windSpeedMPH}</p>
+        <p>windSpeedKMPH: {this.state.windSpeedKMPH}</p>
+        <p>windChillTempF: {this.state.windChillTempF}</p>
+        <p>windChillTempC: {this.state.windChillTempC}</p>
             </div>
         )
     }
