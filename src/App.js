86230f311617be3d505f290f4f1eb71ec3950830
lucassherwin <lksherwin@gmail.com>
2020-07-10 12:55:09 -0400

    gets weather info

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   state = {
     latitude: null,
     longitude: null,
-    userAddress: null
+    userAddress: null,
+    userCityKey: null
   }
 
   getLocation = () =>
@@ -25,10 +26,36 @@ class App extends Component {
   {
     console.log(position)
     this.setState({latitude: position.coords.latitude, longitude: position.coords.longitude})
+    this.getUserAddress();
   }
 
   getUserAddress = () => {
-    
+    let GOOGLEapiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${this.state.latitude},${this.state.longitude}&key=${GOOGLEapiKey}`)
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({userAddress: data.results[0].formatted_address})
+      //address isnt exactly right this might just be for my house though
+      // console.log(data)
+      this.getUserCityID(this.state.userAddress)
+    })
+  }
+
+  //take user address as parameter
+  getUserCityID = (address) => {
+    //weather api key
+    let WEATHERapiKey = process.env.REACT_APP_WEATHER_API_KEY;
+    // console.log(WEATHERapiKey)
+    //get city from address
+    let city = address.split(',');
+    console.log(city[1].replace(/\s+/g, ''));
+    city = city[1].replace(/\s+/g, '')
+    //make api call with city
+    fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${WEATHERapiKey}&q=${city}`)
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({userCityKey: data[0]['Key']})
+    })
   }
 
   //error handling for getting location
@@ -57,17 +84,21 @@ class App extends Component {
 
   render()
   {
-    let apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+    let GOOGLEapiKey = process.env.REACT_APP_GOOGLE_API_KEY;
     return (
       <div className="App">
         {/* <CurrentDay latitude={this.state.latitude} longitude={this.state.longitude} /> */}
         <h1>Location Data:</h1>
         <p>Longitude: {this.state.longitude}</p>
         <p>Latitude: {this.state.latitude}</p>
+        <p>User Address: {this.state.userAddress}</p>
         {
           this.state.latitude && this.state.longitude ? 
-          <img src={`https://maps.googleapis.com/maps/api/staticmap?center=${this.state.latitude},${this.state.longitude}&zoom=14&size=400x300&sensor=false&markers=color:red%7c${this.state.latitude},${this.state.longitude}&key=${apiKey}`} alt='map' />
+          <img src={`https://maps.googleapis.com/maps/api/staticmap?center=${this.state.latitude},${this.state.longitude}&zoom=14&size=400x300&sensor=false&markers=color:red%7c${this.state.latitude},${this.state.longitude}&key=${GOOGLEapiKey}`} alt='map' />
   : null
+        }
+        {
+          this.state.userCityKey !== null ? <CurrentDay cityKey={this.state.userCityKey} /> : null
         }
       </div>
     );
--- src/components/CurrentDay.js
+++ src/components/CurrentDay.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react'
 import { geolocated } from "react-geolocated";
 
 export class CurrentDay extends Component {
+    state = {
+        currentWeatherType: null,
+        currentTempF: null,
+        currentTempC: null,
+        isRaining: null, //this will be a bool
+        seeMoreLink: null,
+        tempHigh: null, //high for the day
+        tempLow: null, //low for the day
+        humidity: null,
+        uvIndex: null,
+        uvIndexText: null,
+        windDirection: null,
+        windSpeedMPH: null,
+        windSpeedKMPH: null,
+        windChillTempF: null,
+        windChillTempC: null
+    }
+    componentDidMount()
+    {
+        // console.log('in currentDay', this.props)
+        let WEATHERapiKey = process.env.REACT_APP_WEATHER_API_KEY;
+        fetch(`http://dataservice.accuweather.com/currentconditions/v1/${this.props.cityKey}?apikey=${WEATHERapiKey}&details=true`)
+        .then(resp => resp.json())
+        .then(data => this.setState({
+        //     currentWeatherType: data[0]['WeatherText'],
+        //     currentTempF: data[0]['Temperature']['Imperial']['Value'],
+        //     currentTempC: data[0]['Temperature']['Metric']['Value'],
+        //     isRaining: data[0]['HasPrecipitation'],
+        //     seeMoreLink: data[0]['Link']
+        }))
+    }
+
     render() {
         return (
             <div>
                 <h1>current day</h1>
+                <p>The Current Weather Is: {this.state.currentWeatherType}</p>
             </div>
         )
     }
